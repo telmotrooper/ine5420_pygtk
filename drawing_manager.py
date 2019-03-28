f6e700162f990ade7fc8efa05e60330ae18f7e23
Telmo Trooper <telmo.trooper@gmail.com>
2019-03-28 09:36:41 -0300

    Drawing logic moved to DrawingManager

---
+++ drawing_manager.py
@@ -0,0 +1,17 @@
+class DrawingManager:
+  def draw_background(self, da, ctx):
+    ctx.set_source_rgb(255, 255, 255) # color white
+    ctx.paint()
+
+  def draw(self, da, ctx):
+    self.draw_background(da, ctx)
+
+    ctx.set_source_rgb(255, 255, 255) # color white
+    ctx.paint()
+
+    ctx.set_source_rgb(0, 0, 0) # color black
+    
+    ctx.set_line_width(2)
+    ctx.move_to(20, 20)
+    ctx.rel_line_to(50,50)
+    ctx.stroke()
--- main.py
+++ main.py
@@ -6,8 +6,11 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 from handler import Handler
+from drawing_manager import DrawingManager
 
 def main():
+  dm = DrawingManager()
+
   builder = Gtk.Builder()
   builder.add_from_file("gui.glade")
   builder.connect_signals(Handler(builder))
@@ -16,21 +19,9 @@ def main():
   window.show_all()
 
   drawing_area = builder.get_object("DrawingArea")
-  drawing_area.connect("draw", draw)
+  drawing_area.connect("draw", dm.draw)
 
   Gtk.main()
-
-def draw(da, ctx):
-  # draw background
-  ctx.set_source_rgb(255, 255, 255) # color white
-  ctx.paint()
-
-  ctx.set_source_rgb(0, 0, 0) # color black
-  
-  ctx.set_line_width(2)
-  ctx.move_to(20, 20)
-  ctx.rel_line_to(50,50)
-  ctx.stroke()
 
 if __name__ == "__main__":
   main()
