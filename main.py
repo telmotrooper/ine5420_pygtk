7709c18f09926f0f0a8a9d576700928235d3c92c
Telmo Trooper <telmo.trooper@gmail.com>
2019-03-28 09:22:13 -0300

    Basic drawing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import gi
-gi.require_version('Gtk', '3.0')
+import cairo
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+
 from handler import Handler
 
 def main():
@@ -13,7 +15,17 @@ def main():
   window = builder.get_object("MainWindow")
   window.show_all()
 
+  drawing_area = builder.get_object("DrawingArea")
+  drawing_area.connect("draw", draw)
+
   Gtk.main()
+
+def draw(da, ctx):
+  ctx.set_source_rgb(0, 0, 0) # color black
+  ctx.set_line_width(2)
+  ctx.move_to(20, 20)
+  ctx.rel_line_to(50,50)
+  ctx.stroke()
 
 if __name__ == "__main__":
   main()
