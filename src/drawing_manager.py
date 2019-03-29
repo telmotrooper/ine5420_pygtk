3154cf3c1c8188e1bc6187536c76862a1fa69d3c
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-29 00:01:27 -0300

    Added polygon

--- src/drawing_manager.py
+++ src/drawing_manager.py
@@ -1,5 +1,6 @@
 from shapes.point import Point
 from shapes.line import Line
+from shapes.polygon import Polygon
 
 class DrawingManager:
   def draw_background(self, da, ctx):
@@ -13,10 +14,23 @@ class DrawingManager:
     ctx.set_line_width(2)
 
     l = Line("Line 1")
-    l.addCoords(100,50)
-    l.addCoords(146,200)
+    l.addCoords(100, 50)
+    l.addCoords(146, 200)
     l.draw(ctx)
 
     p = Point("Point 1")
     p.addCoords(20, 200)  
     p.draw(ctx)
+
+    po = Polygon("Polygon 1")
+    po.addCoords(300, 300)
+    po.addCoords(400, 200)
+    po.addCoords(250, 50)
+    po.draw(ctx)
+
+    po2 = Polygon("Polygon 2")  # fix this: when adding a 2nd polygon it bugs the 1st one
+    po2.addCoords(500, 500)
+    # po2.addCoords(220, 300)
+    # po2.addCoords(250, 100)
+    # po2.addCoords(700, 50)
+    po2.draw(ctx)
--- src/shapes/polygon.py
+++ src/shapes/polygon.py
@@ -1,2 +1,20 @@
 class Polygon:
+  coords = []
 
+  def __init__(self, name):
+    self.name = name
+  
+  def addCoords(self, x, y):
+    self.coords.append(
+      {"x": x, "y": y}
+    )
+
+  def draw(self, ctx):  # reference: https://pycairo.readthedocs.io/
+    ctx.move_to(self.coords[0]["x"], self.coords[0]["y"])
+
+    for entry in self.coords:  # 1st interation does move_to and line_to to same point
+      x2 = entry["x"]
+      y2 = entry["y"]
+      ctx.line_to(x2,y2)
+
+    ctx.stroke()
