d7f19722e5592e06e629c0bdb7b1a21800cf31e7
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-28 23:50:28 -0300

    Line working

--- src/drawing_manager.py
+++ src/drawing_manager.py
@@ -1,22 +1,21 @@
 from shapes.point import Point
+from shapes.line import Line
 
 class DrawingManager:
   def draw_background(self, da, ctx):
-    ctx.set_source_rgb(255, 255, 255) # color white
+    ctx.set_source_rgb(255, 255, 255)  # color white
     ctx.paint()
 
   def draw(self, da, ctx):
     self.draw_background(da, ctx)
 
-    ctx.set_source_rgb(255, 255, 255) # color white
-    ctx.paint()
-
-    ctx.set_source_rgb(0, 0, 0) # color black
-    
+    ctx.set_source_rgb(0, 0, 0)  # color black
     ctx.set_line_width(2)
-    ctx.move_to(20, 20)
-    ctx.rel_line_to(50,50)
-    ctx.stroke()
+
+    l = Line("Line 1")
+    l.addCoords(100,50)
+    l.addCoords(146,200)
+    l.draw(ctx)
 
     p = Point("Point 1")
     p.addCoords(20, 200)  
--- src/shapes/line.py
+++ src/shapes/line.py
@@ -1,2 +1,21 @@
 class Line:
+  coords = []
 
+  def __init__(self, name):
+    self.name = name
+  
+  def addCoords(self, x, y):
+    self.coords.append(
+      {"x": x, "y": y}
+    )
+
+  def draw(self, ctx):  # reference: https://pycairo.readthedocs.io/  
+    x = self.coords[0]["x"]
+    y = self.coords[0]["y"]
+
+    x2 = self.coords[1]["x"]
+    y2 = self.coords[1]["y"]
+
+    ctx.move_to(x,y)
+    ctx.line_to(x2,y2)
+    ctx.stroke()
