dd93e091b49d3f2d62d298e877a0a889d9b44898
Thomas <thomasffeijoo@gmail.com>
2019-03-31 14:20:30 -0300

    added viewport to drawing manager

--- src/drawing_manager.py
+++ src/drawing_manager.py
@@ -2,11 +2,13 @@ from shapes.point import Point
 from shapes.line import Line
 from shapes.polygon import Polygon
 from display_file import DisplayFile
+from viewport import Viewport
 
 class DrawingManager:
   def __init__(self):
     self.display_file = DisplayFile()
     self.draw_counter = 0
+    self.viewport = Viewport()
 
   def draw_background(self, da, ctx):
     ctx.set_source_rgb(255, 255, 255)  # color white
@@ -17,10 +19,11 @@ class DrawingManager:
 
     ctx.set_source_rgb(0, 0, 0)  # color black
     ctx.set_line_width(2)
-    
-    for i in self.display_file.getObjects():
-      print('Drawing object "{}"'.format(i.getName()))
-      i.draw(ctx)
+    if self.display_file.getObjects():
+      '''for i in self.display_file.getObjects():
+        print('Drawing object "{}"'.format(i.getName()))
+        i.draw(ctx)
 
-    self.draw_counter += 1
-    print("draw() #{0}".format(self.draw_counter))
+      self.draw_counter += 1
+      print("draw() #{0}".format(self.draw_counter))'''
+      self.viewport.transformada(ctx)
