d0bb03a0c650e62a20bfc3c5a97c14e81e16872b
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-31 01:18:09 -0300

    Printing every time draw() is called

--- src/drawing_manager.py
+++ src/drawing_manager.py
@@ -6,18 +6,21 @@ from display_file import DisplayFile
 class DrawingManager:
   def __init__(self):
     self.display_file = DisplayFile()
+    self.draw_counter = 0
 
   def draw_background(self, da, ctx):
     ctx.set_source_rgb(255, 255, 255)  # color white
     ctx.paint()
 
   def draw(self, da, ctx):
-    print("I'M DRAWING")
     self.draw_background(da, ctx)
 
     ctx.set_source_rgb(0, 0, 0)  # color black
     ctx.set_line_width(2)
     
     for i in self.display_file.getObjects():
-      print("DRAW OBJECT")
+      print('Drawing object "{}"'.format(i.getName()))
       i.draw(ctx)
+
+    self.draw_counter += 1
+    print("draw() #{0}".format(self.draw_counter))
