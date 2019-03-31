2a435acf905563a54c4d9d6b82837c09659a0beb
Thomas <thomasffeijoo@gmail.com>
2019-03-31 14:29:17 -0300

    changed draw method of point

--- src/shapes/point.py
+++ src/shapes/point.py
@@ -10,13 +10,13 @@ class Point:
 
   def getCoords(self):
     return self.coords
-    
+
   def getName(self):
     return self.name
 
-  def draw(self, ctx):  # Reference: https://pycairo.readthedocs.io/    
-    x = self.coords[0]["x"]
-    y = self.coords[0]["y"]
+  def draw(self, ctx, coords):  # Reference: https://pycairo.readthedocs.io/    
+    x = coords[0]["xViewPort"]
+    y = coords[0]["yViewPort"]
   
     ctx.move_to(x,y)
     ctx.rel_line_to(1,1)  # equivalent to ctx.line_to(x+1,y+1)
