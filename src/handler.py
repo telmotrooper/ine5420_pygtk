aa3dfb45946872feb55c82f8ebf351b2ebc72fee
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-31 01:44:27 -0300

    printToLog() scrolls down log

--- src/handler.py
+++ src/handler.py
@@ -6,10 +6,14 @@ from display_file import DisplayFile
 class Handler:
   def __init__(self, builder, drawingmanager):
     self.builder = builder
+    self.drawingmanager = drawingmanager
+
+    # References to GTK objects
+    self.add_object_window = self.builder.get_object("AddObjectWindow")
     self.text_view = self.builder.get_object("Log")
+
     self.text_buffer = self.text_view.get_buffer()
     self.display_file = DisplayFile()
-    self.drawingmanager = drawingmanager
 
   def onDestroy(self, *args):
     Gtk.main_quit()
@@ -25,8 +29,16 @@ class Handler:
     add_object_window = self.builder.get_object("AddObjectWindow")
     add_object_window.show_all()
 
+  def onAddPolygonPoint(self, button):
+    x_entry = self.builder.get_object("EntryXPolygon").get_text()
+    y_entry = self.builder.get_object("EntryYPolygon").get_text()
+    self.printToLog("onAddPolygonPoint ({},{})".format(x_entry, y_entry))
+
+
+
   def onAddPolygon(self, button):
     self.printToLog("onAddPolygon")
+    self.add_object_window.hide()
 
   def onAddPoint(self, button):
     self.printToLog("onAddPoint")
@@ -37,7 +49,7 @@ class Handler:
 
     p1.addCoords(int(x_entry.get_text()), int(y_entry.get_text()))
     self.display_file.addObject(p1)
-
+    self.add_object_window.hide()
 
   def onAddLine(self, button):
     self.printToLog("onAddLine")
@@ -46,10 +58,13 @@ class Handler:
     y1_entry = self.builder.get_object("EntryY1Line")
     x2_entry = self.builder.get_object("EntryX2Line")
     y2_entry = self.builder.get_object("EntryY2Line")
+
     l1 = Line(name_entry.get_text())
     l1.addCoords(int(x1_entry.get_text()), int(y1_entry.get_text()))
     l1.addCoords(int(x2_entry.get_text()), int(y2_entry.get_text()))
+
     self.display_file.addObject(l1)
+    self.add_object_window.hide()
   
   def onRemoveObjectClicked(self, button):
     self.printToLog("onRemoveObjectClicked")
@@ -67,5 +82,7 @@ class Handler:
     self.printToLog("onMoveWindowRight")
 
   def printToLog(self, text):
-    self.text_buffer.insert_at_cursor(text + "\n")
+    text_buffer, text_view = self.text_buffer, self.text_view
+    text_buffer.insert_at_cursor(text + "\n")
+    text_view.scroll_to_mark(text_buffer.get_insert(), 0, 0, 0, 0)
 
