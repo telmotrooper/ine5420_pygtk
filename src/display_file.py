57b223d5aa76fad213e4eecf837b41aa04a4a033
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-04-02 19:25:43 -0300

    Adding objects to objectList

--- src/display_file.py
+++ src/display_file.py
@@ -3,9 +3,11 @@ from gi.repository import Gtk
 class DisplayFile:
   objects = []
   builder = None
+  objectList = None
 
   def addObject(self, object):
     self.objects.append(object)
+    DisplayFile.objectList.append([object.getName(), "Blabla"])
 
   def getObjects(self):
     return self.objects
@@ -13,7 +15,5 @@ class DisplayFile:
   def setBuilder(self, builder):
     DisplayFile.builder = builder
     DisplayFile.objectList = self.builder.get_object("ObjectList")
-    DisplayFile.objectList.append(["TesteX", "TesteX"])
-    DisplayFile.objectList.append(["Blabla", "Blabla"])
 
 
