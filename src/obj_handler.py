3dab0d6863c2649a6110a38e8fd8c0b7a3315364
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-04-08 21:25:34 -0300

    Added buttons to import and export .obj files

---
+++ src/obj_handler.py
@@ -0,0 +1,9 @@
+class ObjHandler:
+  def __init__(self):
+    print("Hey")
+
+  def importFile(self, path):
+    print("Work in progress")
+  
+  def exportFile(self, path):
+    print("Work in progress")
