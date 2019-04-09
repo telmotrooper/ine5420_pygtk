b39d81103dc13534e57409b8ca234b461d58cc94
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-04-08 22:24:37 -0300

    Iterating through file line by line

--- src/obj_handler.py
+++ src/obj_handler.py
@@ -6,6 +6,10 @@ class ObjHandler:
 
   def importFile(self, path):
     print("{}".format(path))
+    self.file = open(path, "r+")  # read and write
+
+    for line in self.file:
+      print(line)
   
   def exportFile(self, path):
     print("Work in progress")
