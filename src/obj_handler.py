2c6f393e6c8d136624bf3c3af13406fddf5ee34b
Telmo Trooper <telmo.trooper@gmail.com>
2019-04-16 09:51:26 -0300

    Working on implementing line import from .obj

--- src/obj_handler.py
+++ src/obj_handler.py
@@ -15,20 +15,25 @@ class ObjHandler:
     self.file = open(path, "r+")  # read and write
 
     for line in self.file:
-      if(line[0] == "v"):
+      if(line[0] == "v"): # store vertices in a dictionary
         vertice_counter += 1
         vertices[vertice_counter] = line
-      elif(line[0] == "o"):
+
+      elif(line[0] == "o"): # store temporarily the name of the object to come
         match = re.findall(r"\S+", line)
         name = match[1]
+      
       elif(line[0] == "p"): # TODO: FINISH THIS
         match = re.findall(r"\S+", line)
-        print(name)
-      elif(line[0] == "l"): # TODO: FINISH THIS
-        match = re.findall(r"\S+", line)
-        print(name)
+        vertice_for_point = vertices[int(match[1])]
+        match = re.findall(r"\S+", vertice_for_point)
+        coord = { "x": float(match[1]), "y": float(match[2]) }
+        print(coord)
+      # elif(line[0] == "l"): # TODO: FINISH THIS
+      #   match = re.findall(r"\S+", line)
+      #   print(name)
     
-    # print(vertices[1])
+    print(vertices)
     # temp = re.findall(r"\S+", vertices[1])
     # print(temp)
 
