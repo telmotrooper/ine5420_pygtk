67999a9642af2ed8da85c01c8feebaf315979601
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-04-17 22:35:44 -0300

    Most shape methods in shape parent

--- src/shapes/line.py
+++ src/shapes/line.py
@@ -8,45 +8,6 @@ class Line(Shape):
   def __init__(self, name):
     super().__init__(name)
 
-  def getWorldCoords(self):
-    return self.world_coords
-
-  def getNormalizedCoords(self):
-    return self.normalized_coords
-
-  def scaleNormalizedCoords(self, percentage):
-    coords = self.normalized_coords
-
-    for i in range(len(coords)):
-      new_coords = self.transform.scale(
-        coords[i]["x"], coords[i]["y"],
-        percentage, percentage,
-        0, 0
-      )
-      self.normalized_coords[i] = {"x": new_coords[0], "y": new_coords[1]}
-
-  def rotateNormalizedCoords(self, degrees):
-    coords = self.normalized_coords
-
-    for i in range(len(coords)):
-      new_coords = self.transform.rotation(coords[i]["x"], coords[i]["y"], 0, 0, degrees)
-      self.normalized_coords[i] = {"x": new_coords[0], "y": new_coords[1]}
-
-  def setWorldCoords(self, i, x, y):
-    self.world_coords[i] = { "x": x, "y": y }
-    self.normalized_coords[i] = self.transform.normalize(x, y)
-
-  def normalizeCoords(self):
-    for i in range(len(self.world_coords)):
-      x, y = self.world_coords[i]["x"], self.world_coords[i]["y"]
-      self.normalized_coords[i] = self.transform.normalize(x, y)
-
-  def getName(self):
-    return self.name
-
-  def getId(self):
-    return self.id
-
   def drawToViewport(self, ctx, viewport):
     x, y = self.normalized_coords[0]["x"], self.normalized_coords[0]["y"]
     x2, y2 = self.normalized_coords[1]["x"], self.normalized_coords[1]["y"]
