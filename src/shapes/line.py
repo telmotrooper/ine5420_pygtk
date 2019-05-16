5d491f706064b87a7ccdd9216cf5ffd14d55f7ce
Telmo Trooper <telmo.trooper@gmail.com>
2019-05-16 08:54:18 -0300

    Added base class for Curve

--- src/shapes/line.py
+++ src/shapes/line.py
@@ -1,7 +1,3 @@
-import numpy as np
-# pylint: disable=no-name-in-module, import-error
-from utils.gen_random_id import generateRandomId
-from transform import Transform
 from shapes.shape import Shape
 
 class Line(Shape):
--- src/shapes/point.py
+++ src/shapes/point.py
@@ -1,10 +1,5 @@
-import numpy as np
-# pylint: disable=no-name-in-module, import-error
-from utils.gen_random_id import generateRandomId
-from transform import Transform
 from shapes.shape import Shape
 
 class Point(Shape):
   def __init__(self, name):
     super().__init__(name)
-
